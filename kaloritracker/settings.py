b712f9a48e2f9f2aad6b6f406e465fad733b7c1a
Emil Solberg <emil.rafael.solberg@gmail.com>
2022-10-12 19:59:08 +0200

    Fikse template/static directory

--- kaloritracker/settings.py
+++ kaloritracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os.path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,6 +70,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
 
 WSGI_APPLICATION = 'kaloritracker.wsgi.application'
