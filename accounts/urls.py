3162116317e69e33b62156f467a99c9874eb42a5
emsoraffa <emil.rafael.solberg@gmail.com>
2023-01-28 14:12:46 +0100

    Finished

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('accounts/login/', views.login_view, name='login_view'),
-    path('accounts/login/authenticate/', views.authenticate_view, name='authenticate'),
- ]
