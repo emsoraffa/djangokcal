6dbee654ea7f3dd648a53b531f0299d30948cc6e
emsoraffa <emil.rafael.solberg@gmail.com>
2023-01-29 11:42:23 +0100

    added environ var

--- kaloritracker/settings.py
+++ kaloritracker/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-_d0r&ehc+)$q6w^t-fnusthy+m+c#qk!6*i3))&qt61=f)$a@c'
-os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
