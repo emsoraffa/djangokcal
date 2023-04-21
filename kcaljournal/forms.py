4b391c87736a9340697da1c58acac3e743144725
emsoraffa <emil.rafael.solberg@gmail.com>
2023-04-21 13:28:37 +0200

    datepicker

--- kcaljournal/forms.py
+++ kcaljournal/forms.py
@@ -1,12 +1,16 @@
-from django.forms import DateInput, ModelForm
+from django import forms
 from .models import Fooditem,FoodEntry, Journal
 
-class FoodForm(ModelForm):
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class FoodForm(forms.ModelForm):
     class Meta:
         model = Fooditem
         fields='__all__'
 
-class FoodEntryForm(ModelForm):
+class FoodEntryForm(forms.ModelForm):
     class Meta:
         model = FoodEntry
         fields=['food', 'amount', 'date']
