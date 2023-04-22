ad07482e5c5713c1f3f75e09dcf078cc51c16210
emsoraffa <emil.rafael.solberg@gmail.com>
2023-04-22 12:47:11 +0200

    remove old sk

--- kaloritracker/settings.py
+++ kaloritracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-_d0r&ehc+)$q6w^t-fnusthy+m+c#qk!6*i3))&qt61=f)$a@c'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == True)
